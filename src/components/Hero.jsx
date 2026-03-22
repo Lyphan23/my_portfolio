8308da473ebbc6dbe4cf547e1e244211bca0af6a
Lyphan23 <lyphan23022005@gmail.com>
2026-03-22 23:35:51 +0700

    update button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -103,7 +103,10 @@ const Hero = ({ darkMode }) => {
                 data-aos-delay="700"
               >
                 <a href={CV} download className="w-full sm:w-auto">
-                  <button className="w-full sm:w-auto inline-flex items-center justify-center text-white bg-gradient-to-r from-orange-500 to-amber-500 border-0 py-3 px-6 sm:px-8 hover:shadow-[0_0_40px_rgb(255,165,0,0.7)] rounded-full text-base sm:text-lg font-semibold transition-all duration-300 transform">
+                  <button
+                    className="w-full sm:w-auto flex items-center justify-center text-white bg-orange-500 bg-gradient-to-r from-orange-500 to-amber-500 border-0 py-3 px-8 hover:shadow-[0_0_40px_rgba(255,165,0,0.7)] rounded-full text-base sm:text-lg
+                   font-semibold transition-all duration-300 transform active:scale-95 shadow-lg"
+                  >
                     <DownloadIcon className="w-4 h-4 sm:h-5 sm:w-5 mr-2" />
                     Download CV
                   </button>
